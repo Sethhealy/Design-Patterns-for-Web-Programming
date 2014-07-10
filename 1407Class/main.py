93d40bbfa6a92ed894b5795303ed22fa6333f1e6
Sethhealy <sethhealy@Seths-MacBook-Pro.local>
2014-07-10 16:02:48 -0400

    start of project Madlib

---
+++ 1407Class/main.py
@@ -0,0 +1,66 @@
+#comment for single line
+'''
+Name
+Date
+Assignment
+ALWAYS AT THE TOP
+'''
+
+#first_name = "Seth"
+#last_name = "Healy"
+#year_born = 1988
+#age = 2014 - year_born
+#print " I am " + str(age) + " years old"
+# age = raw_input("Type your Age:") #collects information as string
+#
+# print "You are " + age + " years old"
+# budget = 200
+# shoe_price = 2000
+# won_lottery = False
+#
+# if shoe_price <= budget or won_lottery:
+#     print "i can buy my shoes"
+# elif won_lottery:
+#     print "buy them"
+# else:
+#     print "No shoes for you"
+#
+# print "I am done with code"
+# def calc_area(w,h):
+#     a=w * h
+#     return a
+#
+# area = calc_area(50,60)
+# print str(area) + " Sqft"
+#Loops & arrays
+# for i in range(0,10,+3):
+#     print i
+# students = ['studnicky', 'julian', 'seth', 'jordan', 'danny', 'carmine', 'manny-fresh']
+#
+# students.append('alan')
+#
+# for s in students:
+#     print s
+#
+#dictionary
+
+# obj = {"name":["Jordan", "Alan", "Julian"], "age":20, "occupation":"student"}
+#
+# print obj ["name"][2]
+
+#
+# big_string = '''
+# Welcome to the python class, {user_name}.
+# Your going to pass this class
+# '''
+# user_name = "Seth"
+# print big_string.format(**locals())
+#strings that occupy more then 1 line
+
+#tuple = (23, 56)
+
+
+
+
+
+
---
+++ Lab1_Madlib/main.py
@@ -0,0 +1,16 @@
+"""
+Seth Healy
+07/08/14
+Lab 1 Madlib
+ALWAYS AT THE TOP
+"""
+# Here im asking the user to input their first name
+
+first_name = raw_input("Type your First Name: ")
+
+# Here i am asking the user to input their last name
+
+last_name = raw_input("Type your Last Name: ")
+
+# Here im asking for the user to input the year they were born
+
