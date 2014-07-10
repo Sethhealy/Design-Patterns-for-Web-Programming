e779fc2b7153abf090742d6b3a9db69a15253a24
Sethhealy <sethhealy@Seths-MacBook-Pro.local>
2014-07-10 16:05:16 -0400

    committing a new question for if they have kids
    if they do or do not they get a message

--- Lab1_Madlib/main.py
+++ Lab1_Madlib/main.py
@@ -25,3 +25,17 @@ def age_calc(y, b):
 
 #here im calculating the information
 age = age_calc(int(year), 2014)
+
+# here im asking for the number for children you have
+
+children = raw_input("How many children do you have? ")
+
+
+print "Your name is " + first_name + " " + last_name + " and you are " + str(age) + " " + \
+      "years old" + " and you have" + " " + children + " children"
+# Here I have an if statement where if your said you have 0 children a message appears and if you said you
+# did have children another message appeared
+if int(children) == 0:
+    print "Lucky you!!"
+else:
+    print "Lucky you"
