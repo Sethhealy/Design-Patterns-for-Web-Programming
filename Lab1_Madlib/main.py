b7be0e2a077587f28238d04af7e436f0c20abfbb
Sethhealy <sethhealy@Seths-MacBook-Pro.local>
2014-07-10 16:16:17 -0400

    final commit and comments

--- Lab1_Madlib/main.py
+++ Lab1_Madlib/main.py
@@ -2,21 +2,20 @@
 Seth Healy
 07/08/14
 Lab 1 Madlib
-ALWAYS AT THE TOP
 """
-# Here im asking the user to input their first name
+# Here im asking the user to input their first name.
 
 first_name = raw_input("Type your First Name: ")
 
-# Here i am asking the user to input their last name
+# Here i am asking the user to input their last name.
 
 last_name = raw_input("Type your Last Name: ")
 
-# Here im asking for the user to input the year they were born
+# Here im asking for the user to input the year they were born.
 
 year = raw_input("What year were you born ")
 
-# Here im using a function to calculate your age from the output of the previous input field
+# Here im using a function to calculate your age from the output of the previous input field.
 
 
 def age_calc(y, b):
