652cfe7e59589c324bcd82306764f24413c7788c
Sethhealy <sethhealy@Seths-MacBook-Pro.local>
2014-07-15 23:57:48 -0400

    commit a lot of changes 4

---
+++ healy-seth-debt-collector/index.py
@@ -0,0 +1,28 @@
+class Page(object):
+    header = """<!DOCTYPE>
+    <html>
+        <head>
+            <link rel="stylesheet" href="styles/style.css" type="text/css"/>
+            <title>Pre-order today!!! </title>
+        </head>
+        <body>
+
+
+    """
+    content = '''Please Work'''
+    '''
+    <h1> Debt calculator </h1>
+    <p> Everyone goes to school for a better life but we all know when you go as a full time student to where\
+    you cannot work you will acquire a lot of debt here I will show you how much debt a few students are and \
+     what degrees they were in when they graduated </p>
+
+    <h3> (ex)-Students </h3>
+
+    <p> student1 </p>
+    <p> student2 </p>
+    <p> student3 </p>
+    <p> student4 </p>
+    <p> student5 </p>
+
+     </body>
+     </html>'''
--- healy-seth-debt-collector/main.py
+++ healy-seth-debt-collector/main.py
@@ -1,46 +1,131 @@
 import webapp2
+from index import Page
 
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        p = Person()
+        p = PersonPage()
+
+        person1 = Person()
+        person1.cost = 80000
+        person1.name = "Alan"
+        person1.degree = "web Design"
+        person1.grad = 2015
+        person1.year = 1988
+
+        person2 = Person()
+        person2.cost = 90000
+        person2.name = "Seth"
+        person2.degree = "web Design"
+        person2.grad = 2015
+        person2.year = 1988
+
+        person3 = Person()
+        person3.cost = 80000
+        person3.name = "Carmine"
+        person3.degree = "web Design"
+        person3.grad = 2015
+        person3.year = 1988
+
+        person4 = Person()
+        person4.cost = 80000
+        person4.name = "Emmanuel"
+        person4.degree = "web Design"
+        person4.grad = 2015
+        person4.year = 1988
+
+        person5 = Person()
+        person5.cost = 80000
+        person5.name = "Julio"
+        person5.degree = "web Design"
+        person5.grad = 2015
+        person5.year = 1988
+
+        people = {
+            "person1":person1,
+            "person2":person2,
+            "person3":person3,
+            "person4":person4,
+            "person5":person5
+        }
 
         if self.request.GET:
-            p.content = "<div class=\"wrapper\">"
-            p.content += "Name: " + self.request.GET['name']
-            p.content += "Age: " + self.request.GET['age']
-            p.content += "Degree: " + self.request.GET['degree']
-            p.content += "Grad: " + self.request.GET['grad']
-            p.content += "Cost: " + self.request.GET['cost']
-            p.content += "Interest after 5 years: " + self.request.GET['five']
-            p.content += "Interest after 10 years: " + self.request.GET['ten']
-            p.content += "Interest after 15 years: " + self.request.GET['fifteen']
-            p.content += "</div>"
+            if self.request.GET.has_key("person"):
 
+                getPerson = self.request.GET["person"]
 
-class Person(object):
+                p.person = people[getPerson]
+                p.render()
+
+                self.response.write(p.content)
+
+class PersonPage(object):
+
+    person = None;
+
     title = ""
+
     __css = "css/styles.css"
+
     header = """ <!DOCTYPE HTML>
-<head><title></title>
+<head>
+<title>{self.title}</title>
 <link rel="stylesheet" type="text/css" href="{self.css}" />
 </head>
 <body>
 """
-    __content = "welcome peasants"
-    closer = """
-    </body>
+
+    footer = """</body>
 </html>"""
 
+    __content = ""
+
+    def render(self):
+        # generate content
+        self.__content += self.header.format(**locals())
+        self.__content += "<p>"+self.person.name+"</p>"
+        self.__content += "<p>"+str(self.person.year)+"</p>"
+        self.__content += "<p>"+self.person.degree+"</p>"
+        self.__content += "<p>"+str(self.person.grad)+"</p>"
+        self.__content += "<p>"+str(self.person.cost)+"</p>"
+        self.__content += "<p>"+str(self.person.interest_five)+"</p>"
+        self.__content += "<p>"+str(self.person.interest_ten)+"</p>"
+        self.__content += "<p>"+str(self.person.interest_fifteen)+"</p>"
+        self.__content += self.footer
+
+    @property
+    def content(self):
+        return self.__content
+
+    @property
+    def css(self):
+        return self.__css
+
+
+
+class Person(object):
     def __init__(self):
         self.name = ""
-        self.age = 0
+        self.year = 0
         self.degree = ""
-        self.grad = ""
-        self.cost = ""
+        self.grad = 0
+        self.cost = 0
 
-    def print_out(self):
-        return self.name + str(self.age) + self.degree + self.grad + self.cost
+    @property
+    def interest_five(self):
+        return .0466 * 5 * self.cost
+
+    @property
+    def interest_ten(self):
+        return .0466 * 10 * self.cost
+
+    @property
+    def interest_fifteen(self):
+        return .0466 * 15 * self.cost
+
+    @property
+    def age(self):
+        return 2014 - self.year
 
 
 app = webapp2.WSGIApplication([
