45998c8737038ea832e81b734e7b72df06f1c5fb
Sethhealy <sethhealy@Seths-MacBook-Pro.local>
2014-07-15 21:51:52 -0400

    commit 2
    adding people

--- healy-seth-debt-collector/main.py
+++ healy-seth-debt-collector/main.py
@@ -2,7 +2,13 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        p = Person()
+        #send in content we want
+        p.content = "Sign the form:"
+        p.css = "css/styles.css"
+        p.title = "HI"
+        #print out the content
+        self.response.write(p.print_out())
 
 
 
@@ -10,6 +16,29 @@ class MainHandler(webapp2.RequestHandler):
 
 
 class Person(object):
+
+    title =""
+    __css ="css/styles.css"
+    header = """ <!DOCTYPE HTML>
+<head><title></title>
+<link rel="stylesheet" type="text/css" href="{self.css}" />
+</head>
+<body>
+"""
+    __content = "welcome peasants"
+    closer = """
+    </body>
+</html>"""
+
+    def __init__(self):
+        self.name = ""
+        self.age = 0
+        self.degree = ""
+        self.grad = ""
+        self.cost = ""
+
+    def print_out(self):
+        return self.name + str(self.age) + self.degree + self.grad + self.cost
 
 
 
