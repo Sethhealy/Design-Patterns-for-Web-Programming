19e1a157ac03ef51da12390e4f7ccc50ec498abd
Sethhealy <sethhealy@Seths-MacBook-Pro.local>
2014-07-15 22:03:15 -0400

    commit third
    adding content

--- healy-seth-debt-collector/main.py
+++ healy-seth-debt-collector/main.py
@@ -1,24 +1,26 @@
 import webapp2
 
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         p = Person()
-        #send in content we want
-        p.content = "Sign the form:"
-        p.css = "css/styles.css"
-        p.title = "HI"
-        #print out the content
-        self.response.write(p.print_out())
 
-
-
-
+        if self.request.GET:
+            p.content = "<div class=\"wrapper\">"
+            p.content += "Name: " + self.request.GET['name']
+            p.content += "Age: " + self.request.GET['age']
+            p.content += "Degree: " + self.request.GET['degree']
+            p.content += "Grad: " + self.request.GET['grad']
+            p.content += "Cost: " + self.request.GET['cost']
+            p.content += "Interest after 5 years: " + self.request.GET['five']
+            p.content += "Interest after 10 years: " + self.request.GET['ten']
+            p.content += "Interest after 15 years: " + self.request.GET['fifteen']
+            p.content += "</div>"
 
 
 class Person(object):
-
-    title =""
-    __css ="css/styles.css"
+    title = ""
+    __css = "css/styles.css"
     header = """ <!DOCTYPE HTML>
 <head><title></title>
 <link rel="stylesheet" type="text/css" href="{self.css}" />
@@ -41,9 +43,6 @@ class Person(object):
         return self.name + str(self.age) + self.degree + self.grad + self.cost
 
 
-
-
-
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+                                  ('/', MainHandler)
+                              ], debug=True)
