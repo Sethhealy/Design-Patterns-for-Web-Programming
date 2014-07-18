fce4b34669502712924d972ab1ed34501af627f2
Sethhealy <sethhealy@Seths-MacBook-Pro.local>
2014-07-17 22:19:00 -0400

    finished quiz
    i added a few changes but mainly added content

--- healy-seth-quizclasses/main.py
+++ healy-seth-quizclasses/main.py
@@ -5,6 +5,14 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
 
+class Student(object):
+    def __init__(self):
+        self.name = ""
+        self.year_completed = 0
+        self.grad = ""
+        self.odds= 0
+        self.chances = 0
+
 
 class Page(object):
     _head = """<!DOCTYPE HTML>
@@ -12,14 +20,23 @@ class Page(object):
     <title>Inheritance</title>
 </head>
 <body>"""
+
+    """<p> Everyone goes to school for a better life but not everyone goes to school and i know not all drop outs \
+        go on to be in the military or become a custodian but the odds are better then their favor </p><br/>
+        if you go to school for the full 4 years and serve 8 years in the military it will be {chances} until your done. <br/>
+        giving you decide to quit school and become a custodian you will get out of school if you do the full schooling in {odds}
+        """
     __content = ''
 
     close = """
 </body>
 </html>"""
 
-
 class Custodian(Page):
+    '''
+    Here im calculating how long of school you need to graduate from school and start your field of choice if the choice
+    is custodial work.
+    '''
     def odds(self):
         return 4 + 0
 
@@ -30,14 +47,15 @@ class Custodian(Page):
     def print_out(self):
         return self._head + self.__content + self.close
 
-    @content.setter
-    def content(self, c):
-        self.__content = c
 
 
 class Solider(Page):
+    '''
+    This is how long it will take until you have a civilian live and out of general school (public school)
+    '''
     def chances(self):
-        return 4 + 4
+        return 4 + 8
+
 
 
 app = webapp2.WSGIApplication([
