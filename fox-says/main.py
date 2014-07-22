c0f1dcfaa5e732c2aeef0eca31f1dba9996842bf
Sethhealy <sethhealy@Seths-MacBook-Pro.local>
2014-07-21 21:56:02 -0400

    commit
    fixed a few problems i had. I was making the wrong names for the
    animals. Supposed to make classes

--- fox-says/main.py
+++ fox-says/main.py
@@ -4,38 +4,9 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         A = AnimalPage()
 
-        animal1 = Animal()
-        animal1.name = "Fox"
-        animal1.phylum = " "
-        animal1.classification = " "
-        animal1.family = ""
-        animal1.genus = ""
-        animal1.average = 0
-        animal1.habitat = ""
-        animal1.geo = ""
 
-
-        animal2 = Animal()
-        animal2.name = "Fox"
-        animal2.phylum = " "
-        animal2.classification = " "
-        animal2.family = ""
-        animal2.genus = ""
-        animal2.average = 0
-        animal2.habitat = ""
-        animal2.geo = ""
-
-
-
-        animal3 = Animal()
-        animal3.name = "Fox"
-        animal3.phylum = " "
-        animal3.classification = " "
-        animal3.family = ""
-        animal3.genus = ""
-        animal3.average = 0
-        animal3.habitat = ""
-        animal3.geo = ""
+        fox = Fox()
+        self.response.write(fox.roar())
 
 
 
@@ -59,8 +30,13 @@ class AnimalPage(object):
 
 
 
+
 class Animal(object):
+
     def __init__(self):
+
+        self._sound = "meow"
+
         self.name = ""
         self.phylum = ""
         self.classification = ""
@@ -69,6 +45,54 @@ class Animal(object):
         self.average = 0
         self.habitat = ""
         self.geo = ""
+
+    def roar(self):
+        return self._sound
+
+
+
+class Fox(Animal):
+
+    def __init__(self):
+        self.name = "Fox"
+        self.phylum = ""
+        self.classification = ""
+        self.family = ""
+        self.genus = ""
+        self.average = 0
+        self.habitat = ""
+        self.geo = ""
+        self._sound = "asd"
+
+
+class Cow(Animal):
+
+    def __init__(self):
+        self.name = "Cow"
+        self.phylum = ""
+        self.classification = ""
+        self.family = ""
+        self.genus = ""
+        self.average = 0
+        self.habitat = ""
+        self.geo = ""
+        self._sound = "woof"
+
+
+
+class Horse(Animal):
+
+    def __init__(self):
+        self.name = "Horse"
+        self.phylum = ""
+        self.classification = ""
+        self.family = ""
+        self.genus = ""
+        self.average = 0
+        self.habitat = ""
+        self.geo = ""
+        self._sound = "woof"
+
 
 
 app = webapp2.WSGIApplication([
