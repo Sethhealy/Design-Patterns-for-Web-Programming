493fb22f91ca13251823a26ac6d89aab9dd33dff
Sethhealy <sethhealy@seths-mbp.barrynet.barry.edu>
2014-07-22 13:41:51 -0400

    commit changes to class
    I’ve added some html to the classes for displaying purposes

--- fox-says/main.py
+++ fox-says/main.py
@@ -2,17 +2,34 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        A = AnimalPage()
 
-        horse=Horse()
-        cow=Cow()
-        fox = Fox()
-        self.response.write(fox.roar())
+        animalPage = AnimalPage()
+
+        animal = Animal()
+
+        if self.request.GET:
+
+            if self.request.GET.has_key("animal"):
+
+                if self.request.GET["animal"] == "horse":
+                    animal = Horse()
+
+                if self.request.GET["animal"] == "cow":
+                    animal = Cow()
+
+                if self.request.GET["animal"] == "fox":
+                    animal = Fox()
+
+        animalPage._content = animal.roar()
+
+        self.response.write(animalPage.print_out())
 
 
 
 class AnimalPage(object):
-    _head = """<!DOCTYPE HTML>
+
+    person= None
+    _header = """<!DOCTYPE HTML>
 <head>
     <title>What Does the Fox Say</title>
 </head>
@@ -27,8 +44,28 @@ class AnimalPage(object):
         pass
 
     def print_out(self):
-        return self._head + self._content + self._close
+        return self._header + self._content + self._close
 
+
+    def render(self):
+        # generate content
+        self._content += self.header.format(**locals())
+
+
+
+        if bool(self.animal):
+            self._content += "<div class='names'>"
+            self._content += "<p>" + "Animal name: " + self.animal.name + "</p>"
+            self._content += "<p>" + "Phylum: " + self.animal.phylum + "</p>"
+            self._content += "<p>" + "Classification: " + self.animal.classification + "</p>"
+            self._content += "<p>" + "Family: " + self.animal.family + "</p>"
+            self._content += "<p>" + "Genus: " + self.animal.genus + "</p>"
+            self._content += "<p>" + "Average: " + str(self.animal.average) + "</p>"
+            self._content += "<p>" + "Habitat: " + self.animal.habitat + "</p>"
+            self._content += "<p>" + "Geo: " + self.animal.geo +"</p>"
+            self._content += "<p>" + "Sound: " + self.animal._sound +"</p>"
+            self._content += "</div>"
+        self._content += self.footer
 
 
 
@@ -77,7 +114,7 @@ class Cow(Animal):
         self.average = 0
         self.habitat = ""
         self.geo = ""
-        self._sound = "woof"
+        self._sound = "moo"
 
 
 
