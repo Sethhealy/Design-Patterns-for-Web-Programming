df497c4724e3d925c717e5699523f4b5ba3b3892
Sethhealy <sethhealy@Seths-MacBook-Pro.local>
2014-07-22 22:04:27 -0400

    fixing errors
    im trying to fix a few errors i created

--- inheritancequiz/main.py
+++ inheritancequiz/main.py
@@ -1,15 +1,14 @@
 import webapp2
 
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         s = Submission()
-        s._inputs=[{'type':'text', 'placeholder':'First Name', 'name':'fname'},
-                  {'type':'text', 'placeholder':'Last Name', 'name':'lname'},
-                {'type':'text','placeholder':'Email','name':'email'},
-                {'type':'submit', 'name':'submit', 'value':'Go'}]
+        s._inputs = [{'type': 'text', 'placeholder': 'First Name', 'name': 'fname'},
+                     {'type': 'text', 'placeholder': 'Last Name', 'name': 'lname'},
+                     {'type': 'text', 'placeholder': 'Email', 'name': 'email'},
+                     {'type': 'submit', 'name': 'submit', 'value': 'Go'}]
         self.response.write(s.print_out())
-
-
 
 
 class Display(object):
@@ -24,18 +23,21 @@ class Display(object):
 </body>
 </html>"""
 
+
 class Submission(Display):
-
-
-    _inputs=''
-    form_start="<form method=GET action=""/>"
+    _inputs = ''
+    form_start = "<form method=\"GET\" action=""/>"
     # form_end="</form>"
 
     def __init__(self):
         Display.__init__(self)
 
 
+    def print_out(self):
+        return self._head + self.form_start + \
+               self._close
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+                                  ('/', MainHandler)
+                              ], debug=True)
