dbf8552aa56ded3e50ef010ae801da87b29c4263
Sethhealy <sethhealy@Seths-MacBook-Pro.local>
2014-07-22 21:55:55 -0400

    adding classes and 1 function
    i added all the classes and 1 function but i passed on it for now I’m
    not sure why i made it actually

--- inheritancequiz/main.py
+++ inheritancequiz/main.py
@@ -2,7 +2,42 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        s = Submission()
+        s.inputs=[{'type':'text', 'placeholder':'First Name', 'name':'fname'},
+                  {'type':'text', 'placeholder':'Last Name', 'name':'lname'},
+                {'type':'text','placeholder':'Email','name':'email'},
+                {'type':'submit', 'name':'submit', 'value':'Go'}]
+        self.response.write(s.print_out())
+
+
+
+
+class Display(object):
+    _head = """<!DOCTYPE HTML>
+<head>
+    <title>Inheritance</title>
+</head>
+<body>"""
+    _content = ''
+
+    _close = """
+</body>
+</html>"""
+
+    def __init__(self):
+        pass
+
+
+class Submission(Display):
+
+
+
+
+
+
+
+
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
