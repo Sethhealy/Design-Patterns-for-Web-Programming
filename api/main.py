e4a46f41957ee4c9738df86574c060ba3e3b57b4
Sethhealy <sethhealy@Seths-MacBook-Pro.local>
2014-07-28 17:41:46 -0400

    commit fourth
    fixing a few things with my model

--- api/main.py
+++ api/main.py
@@ -19,47 +19,62 @@ class MainHandler(webapp2.RequestHandler):
 
         print jsondoc['genres'][0]['id']
 
+
+        movieModel = Moviemodel()
+        movie = movieModel.movie('title')
+
+        page = Page()
+
+        movieView = MovieView()
+        movieView.movie = movie
+
+        page._content = movieView.content
+
+        self.response.write(page.print_out())
+
+
 class MovieView(object):
     def __init__(self):
-        self.__dos = []
+        self.__movie
         self.content = ''
 
     @property
-    def dos(self):
+    def movie(self):
         pass
 
-    @dos.setter
-    def dos(self, arr):
-        self.__dos = arr
+    @movie.setter
+    def movie(self, movie):
+        self.__movie = movie
         self.create_display()
 
 
     def create_display(self):
-        for do in self.__dos:
-            self.content += "Title: " + do.title
-            self.content += "Vote average: " + do.vote_average
-            self.content += "Vote count: " + do.vote_count
-            self.content += "(" + do.release_date + ')'
-            self.content += "Overview: " + do.overview
-            self.content += "<img src=\"images/" + do.backdrop_path + '.jpg" />'
-            self.content += "<br />"
-            self.content += "Tagline: " + do.tagline
-            self.content += "(" + do.budget + ')'
-            self.content += "High: " + do.runtime
-            self.content += "<img src=\"images/" + do.poster_path + '.jpg" />'
-            self.content += "<br />"
-            self.content += "Adult: " + do.adult
-            self.content += "(" + do.revenue + ')'
-            self.content += "Status: " + do.status
-            self.content += "Status: " + do.belongs_to_collection
-            self.content += "<br />"
-            self.content += "Popularity: " + do.popularity
-            self.content += "Spoken Language: " + do.spoken_language
-            self.content += "Production Companies: " + do.production_companies
-            self.content += "Production countries: " + do.production_countries
-
+        self.content += "Title: " + self.__movie.title
+        self.content += "Vote average: " + self.__movie.vote_average
+        self.content += "Vote count: " + self.__movie.vote_count
+        self.content += "(" + self.__movie.release_date + ')'
+        self.content += "Overview: " + self.__movie.overview
+        self.content += "<img src=\"images/" + self.__movie.backdrop_path + '.jpg" />'
+        self.content += "<br />"
+        self.content += "Tagline: " + self.__movie.tagline
+        self.content += "(" + self.__movie.budget + ')'
+        self.content += "High: " + self.__movie.runtime
+        self.content += "<img src=\"images/" + self.__movie.poster_path + '.jpg" />'
+        self.content += "<br />"
+        self.content += "Adult: " + self.__movie.adult
+        self.content += "(" + self.__movie.revenue + ')'
+        self.content += "Status: " + self.__movie.status
+        self.content += "Status: " + self.__movie.belongs_to_collection
+        self.content += "<br />"
+        self.content += "Popularity: " + self.__movie.popularity
+        self.content += "Spoken Language: " + self.__movie.spoken_language
+        self.content += "Production Companies: " + self.__movie.production_companies
+        self.content += "Production countries: " + self.__movie.production_countries
+        self.content += "Status: " + self.__movie.status
+        self.content += "<a href=" + self.__movie.homepage + '</a>'
 
         print self.content
+
 
 class Moviemodel(object):
     """THis is the model class. it is sending a request to the yahoo api and getting
@@ -68,11 +83,18 @@ class Moviemodel(object):
     def __init__(self):
         self.url = "https://api.themoviedb.org/3/movie/550?api_key=9ada58564fcdacbd21d0aca3ec33f0f1"
 
+    # create a request to send to server
+        request = urllib2.Request(self.url)
+        opener = urllib2.build_opener()
+        # sends the request and gets response
+        self.data = opener.open(request)
+
+
+    def movie(self, title):
+        pass
 
 
 class MovieDataObject(object):
-
-
     def __init__(self):
         self.adult = ""
         self.backdrop_path = ""
@@ -97,8 +119,6 @@ class MovieDataObject(object):
         self.title = ""
         self.vote_average = 0
         self.vote_count = 0
-
-
 
 
 class Page(object):
---
+++ dpw/Healy_Seth_madlib/Lab1_Madlib/main.py
@@ -0,0 +1,71 @@
+"""
+Seth Healy
+07/08/14
+Lab 1 Madlib
+"""
+# Here im asking the user to input their first name.
+
+first_name = raw_input("Type your First Name: ")
+
+# Here i am asking the user to input their last name.
+
+last_name = raw_input("Type your Last Name: ")
+
+# Here im asking for the user to input the year they were born.
+
+year = raw_input("What year were you born ")
+
+# Here im using a function to calculate your age from the output of the previous input field.
+
+
+def age_calc(y, b):
+    a = b - y
+    return a
+
+#here im calculating the information for your age.
+age = age_calc(int(year), 2014)
+
+# here im asking for the number for children you have.
+
+children = raw_input("How many children do you have? ")
+
+
+print "Your name is " + first_name + " " + last_name + " and you are " + str(age) + " " + \
+      "years old" + " and you have" + " " + children + " children"
+# Here I have an if statement where if your said you have 0 children a message appears and if you said you
+# did have children another message appeared. I added if statements for a fun message.
+if int(children) == 0:
+    print "Lucky you!!"
+else:
+    print "Lucky you"
+
+# Here im having the user pick a number for there hipster name between 0-2 because 0 is the first location.
+number = raw_input("Pick a number between 0-2 for your hipster name: ")
+
+# Here im outputting the hipster name from the number the user chose.
+name = ["smooth", "sparkly", "Funky fresh"]
+
+print "Your hipster name is " + name[int(number)] + ' ' + first_name
+# I made a dictionary and named it obj where i am pulling my variable first_name and my variable name with another
+# variable for number pulling it into a string naming your hipster name so you can know how cool you are.
+
+obj = {
+    "first_name": first_name,
+    "name": name[int(number)]
+}
+
+print "Or maybe it is " + obj["first_name"] + " " + obj["name"]
+
+# This starts my loop where im looping a never ending song 6 times and outputting it.
+song = '''
+Welcome {user} To the sentence that never ends
+It goes on and on my friend
+ok well not forever
+'''
+
+output_sting = ''
+user = last_name
+for i in range(0, 6):
+    output_sting += song
+
+print output_sting.format(**locals())
