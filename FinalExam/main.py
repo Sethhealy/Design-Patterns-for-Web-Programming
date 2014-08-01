8af2302d45db5b4a1e0cd12077a56d4d1e4f5a61
Sethhealy <sethhealy@Seths-MacBook-Pro.local>
2014-07-31 22:08:03 -0400

    commit

--- FinalExam/main.py
+++ FinalExam/main.py
@@ -1,56 +1,71 @@
-'''
+"""
 Seth Healy
 Final Exam
 07/31/14
-'''
+"""
 
 import json
 import webapp2
-from urllib2 import urlopen, Request
+from urllib2 import urlopen, Request, build_opener
 
 
 class MainHandler(webapp2.RequestHandler):
-    #im getting my requests so that i can display the information
+    # im getting my requests so that i can display the information
     def get(self):
         if self.request.GET:
+            p = Page()
 
-            p=Page()
+            musical = self.request.GET['music']
 
-            music = self.request.GET['music']
             mm = musicModel()
-            searchdata = mm.music(music)
+            searchdata = mm.songs(self, musical)
             mv = musicView()
             mv.music = searchdata
-            print(p._content)
 
-#this is where all my viewable data is contained
+
+
+# this is where all my viewable data is contained
 class musicView(object):
     def __init__(self):
         self.__music = musicDataObject()
 
-#this is my model where all my data is contained.
-class musicModel(object):
-    def music(self):
-        musicrequest = Request('http://rebeccacarroll.com/api/music/music.json')
-        musicresponse = urlopen(musicrequest)
-        musicObject = json.load(musicresponse)
 
-#calling my dataobject using these.
-        do = musicDataObject()
-        do.musicObject['title'] = self.title
-        do.musicObject['artist'] = self.artist
-        do.musicObject['length'] = self.length
-        do.musicObject['year'] = self.year
-        do.musicObject['label'] = self.label
-        do.musicObject['cover'] = self.cover
-        do.musicObject['file'] = self.file
-        return do
+# this is my model where all my data is contained.
+class musicModel(object):
+    def __init__(self, songs):
+        self.__music = songs
+
+        def song(self):
+            musicrequest = Request('http://rebeccacarroll.com/api/music/music.json')
+            #build the request
+            request = Request("http://rebeccacarroll.com/api/music/music.json")
+
+            # create an object that fetches pages for us
+            opener = build_opener()
+
+            #tell object what to fetch
+            music = opener.open(request)
+            musicresponse = urlopen(musicrequest)
+            musicObject = json.load(musicresponse)
+
+            #calling my dataobject using these.
+            do = musicObject()
+            do.musicObject = self.title
+            do.musicObject = self.artist
+            do.musicObject = self.length
+            do.musicObject = self.year
+            do.musicObject = self.label
+            do.musicObject = self.cover
+            do.musicObject = self.file
+            return do
+
 
 #creating my dataobject class where i can define all the and pull all the json information.
 
 
 class musicDataObject(object):
     def __init__(self):
+        self.musicObject = None
         self.title = ''
         self.artist = ''
         self.length = 0
@@ -64,11 +79,11 @@ class musicDataObject(object):
 class Page(object):
     _head = """<!DOCTYPE HTML>
 <head>
-    <title> Final exam</title>
+    <title> Final exam </title>
 </head>
 <body>"""
     _content = '''
-    <h1> Top 10 pop hits </h1>
+    <h1> Top 10 Pop Hits </h1>
     <a href="#"><button> Like a Rolling Stone </button></a>
     <a href="#"><button> Satisfaction </button></a>
     <a href="#"><button> Imagine </button></a>
